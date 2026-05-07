4d1172ed9336bd70a073dde187bad8684b5ca193
EndDavid <120802509+EndDavid@users.noreply.github.com>
2026-05-07 17:06:09 +0800

    Add files via upload

--- codevar_generator.c
+++ codevar_generator.c
@@ -4,6 +4,34 @@
 #include <stdio.h>
 #include <string.h>
 
+#include <stdio.h>
+
+void print_escaped_string(const char *str) {
+    if (str == NULL) return;
+    
+    for (const char *p = str; *p != '\0'; ++p) {
+        switch ((unsigned char)*p) {
+            case '\a': printf("\\a"); break;
+            case '\b': printf("\\b"); break;
+            case '\f': printf("\\f"); break;
+            case '\n': printf("\\n"); break;
+            case '\r': printf("\\r"); break;
+            case '\t': printf("\\t"); break;
+            case '\v': printf("\\v"); break;
+            case '\\': printf("\\\\"); break;
+            case '\'': printf("\\'"); break;
+            case '\"': printf("\\\""); break;
+            default:
+                if (*p < 32 || *p == 127) {
+                    printf("\\x%02x", (unsigned char)*p);
+                } else {
+                    putchar(*p);
+                }
+                break;
+        }
+    }
+}
+
 char line[1024] = "";
 int main() {
     freopen("cppcode.cpp", "r", stdin);
@@ -14,7 +42,10 @@ int main() {
 	puts("    \"#include <cstdlib>\\n\"");
 
 	while (gets(line)) {
-		printf("    \"%s\\n\"\n", line);
+		//printf("    \"%s\\n\"\n", line);
+		printf("	\"");
+		print_escaped_string(line);
+		printf("\\n\"\n");
 
 		if(strstr(line, "int main()")) {
 			printf("    \"    freopen(\\\"fr.in\\\", \\\"r\\\", stdin);\\n\"\n");
