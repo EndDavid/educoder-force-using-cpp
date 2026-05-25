f16c03d615c337567941a03ba84b1265aa938e75
EndDavid <120802509+EndDavid@users.noreply.github.com>
2026-05-25 17:47:53 +0800

    2026.5.25 增加python实现

---
+++ educoder_force_using_cpp_python_implementation/codevar_generator.c
@@ -0,0 +1,54 @@
+// Use for generate cppcode variable.
+// If "copy & paste" were prohibited in educoder editor, you should not use codevar_generator. Please write code manually. (Imaging you are this generator XD)
+
+#include <stdio.h>
+#include <string.h>
+
+void print_escaped_string(const char *str) {
+    if (str == NULL) return;
+    
+    for (const char *p = str; *p != '\0'; ++p) {
+        switch ((unsigned char)*p) {
+            case '\a': printf("\\a"); break;
+            case '\b': printf("\\b"); break;
+            case '\f': printf("\\f"); break;
+            case '\n': printf("\\n"); break;
+            case '\r': printf("\\r"); break;
+            case '\t': printf("\\t"); break;
+            case '\v': printf("\\v"); break;
+            case '\\': printf("\\\\"); break;
+            case '\'': printf("\\'"); break;
+            case '\"': printf("\\\""); break;
+            default:
+                if (*p < 32 || *p == 127) {
+                    printf("\\x%02x", (unsigned char)*p);
+                } else {
+                    putchar(*p);
+                }
+                break;
+        }
+    }
+}
+
+char line[1024] = "";
+int main() {
+    freopen("cppcode.cpp", "r", stdin);
+    freopen("codevar.py", "w", stdout);
+
+    puts("cppcode = \"\"\"");
+	puts("#include <cstdio>\\n");
+	puts("#include <cstdlib>\\n");
+
+	while (gets(line)) {
+		//printf("    \"%s\\n\"\n", line);
+		print_escaped_string(line);
+		printf("\\n\n");
+
+		if(strstr(line, "int main()")) {
+			printf("    freopen(\\\"fr.in\\\", \\\"r\\\", stdin);\\n\n");
+		}
+	}
+
+	puts("\"\"\"");
+	return 0;
+}
